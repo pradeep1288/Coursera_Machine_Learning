63c7f0f41e14c2b0bdbb5a857d44e642c1058036
Pradeep Nayak <pradeepu@netapp.com>
2016-06-07 16:34:34 -0700

    multi-variate feature normalization done

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -26,13 +26,13 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
+mu = mean(X);
 
-
-
-
-
-
-
+for i = 1:size(X,2)
+    X_norm(:,i) = X_norm(:,i) - mu(i);
+    sigma(i) = std(X_norm(:,i));
+    X_norm(:,i) = X_norm(:,i) / sigma(i);
+end
 
 % ============================================================
 
